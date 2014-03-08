4ba818534f0f3ba927d5ea352a17084f86110a52
orenbest <orenbest@gmail.com>
2014-03-08 16:07:04 +0200

    Areas added for customer and admin

---
+++ GazProjec/Areas/Admin/Controllers/BillsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GazProjec.Areas.Admin.Controllers
+{
+    public class BillsController : Controller
+    {
+        //
+        // GET: /Admin/Bills/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ GazProjec/Areas/Customers/Controllers/BillsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GazProjec.Areas.Customers
+{
+    public class BillsController : Controller
+    {
+        //
+        // GET: /Customers/Bills/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
