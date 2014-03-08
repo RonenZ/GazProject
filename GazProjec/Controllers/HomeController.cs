0d9144b5ba0875fc847363fc44db8033eedd7455
orenbest <orenbest@gmail.com>
2014-03-08 22:31:49 +0200

    More hebrew changes and some layout changes

--- GazProjec/Controllers/HomeController.cs
+++ GazProjec/Controllers/HomeController.cs
@@ -21,15 +21,11 @@ namespace GazProjec.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
