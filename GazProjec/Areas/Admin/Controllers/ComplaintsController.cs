07f1e51356993874e8da7f9e943ee38a104d3e03
orenbest <orenbest@gmail.com>
2014-04-14 15:43:13 +0300

    added admin complaints

--- GazProjec/Areas/Admin/Controllers/ComplaintsController.cs
+++ GazProjec/Areas/Admin/Controllers/ComplaintsController.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Gaz.DAL;
+using GazProjec.Areas.Admin.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 
 namespace GazProjec.Areas.Admin.Controllers
 {
@@ -13,7 +15,47 @@ namespace GazProjec.Areas.Admin.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View(GetData());
+        }
+
+
+        public List<ComplaintModel> GetData()
+        {
+            using (var db = new GazDBContext())
+            {
+                return db.UserComplaints.Select(o => new ComplaintModel
+                {
+                    CounterID = o.CounterID,
+                    ComplaintDescription = o.ComplaintDescription,
+                    ComplaintID = o.ID,
+                    CreateTime = o.CreateTime,
+                    Disable = o.Disable,
+                    UserID = o.UserID
+                }).Where(o => o.Disable == false).ToList();
+            }
+        }
+
+        public ActionResult ComplaintRead([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetData(), JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult ComplaintUpdate([DataSourceRequest] DataSourceRequest request, ComplaintModel comp)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new GazDBContext())
+                {
+                    var result = db.UserComplaints.Single(o => o.ID == comp.ComplaintID);
+
+                    result.Disable = comp.Disable;
+
+                    db.SaveChanges();
+                }
+            }
+
+            return Json(new[] { comp }.ToDataSourceResult(request, ModelState));
         }
 
     }
---
+++ GazProjec/Areas/Admin/Models/ComplaintModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GazProjec.Areas.Admin.Models
+{
+    public class ComplaintModel
+    {
+      public int ComplaintID { get; set; }
+      public string ComplaintDescription  { get; set; }
+      public int UserID { get; set; }
+      public int CounterID { get; set; }
+      public DateTime CreateTime { get; set; }
+      public bool Disable { get; set; }
+    }
+}
