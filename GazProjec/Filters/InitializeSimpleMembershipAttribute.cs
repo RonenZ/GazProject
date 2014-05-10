0670b3d1dc7f220799d416ee585571d413fa865c
orenbest <orenbest@gmail.com>
2014-05-10 21:37:42 +0300

    database fix

--- GazProjec/Filters/InitializeSimpleMembershipAttribute.cs
+++ GazProjec/Filters/InitializeSimpleMembershipAttribute.cs
@@ -18,7 +18,10 @@ namespace GazProjec.Filters
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             // Ensure ASP.NET Simple Membership is initialized only once per app start
-            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+            if (!WebSecurity.Initialized)
+            {
+                LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+            }
         }
 
         private class SimpleMembershipInitializer
